c3bd38fae42882774b4faa7eb5fc53e8e095ac23
Nikol Petrunova <nikol@mailerlite.com>
2018-11-23 13:54:26 +0000

    added validation of url format

--- includes/class.woo-mailerlite-integration.php
+++ includes/class.woo-mailerlite-integration.php
@@ -72,9 +72,10 @@ if ( ! class_exists( 'Woo_Mailerlite_Integration' ) ) :
                     'default'           => '',
                 ),
                 'store' => array(
-                    'title'             => __( 'Store website', 'woo-mailerlite' ),
+                    'title'             => __( 'Shop website', 'woo-mailerlite' ),
                     'type'              => 'text',
-                    'desc_tip'          => false,
+                    'description' => __( 'Format should be https://yourshop.com', 'woo-mailerlite' ),
+                    'desc_tip'          => true,
                     'default'           => '',
                 ),
                 'group' => array(
@@ -272,8 +273,15 @@ if ( ! class_exists( 'Woo_Mailerlite_Integration' ) ) :
                 }
             }
 
-            if (isset($settings['consumer_key']) && isset($settings['consumer_secret'])) {
-                mailerlite_wp_set_consumer_data( $settings['consumer_key'], $settings['consumer_secret'], $settings['store'], $settings['api_key']);
+            if (isset($settings['consumer_key']) && isset($settings['consumer_secret']) && isset($settings['store'])) {
+                if (strpos($settings['store'], 'https://') !== false) {
+                    mailerlite_wp_set_consumer_data( $settings['consumer_key'], $settings['consumer_secret'], $settings['store'], $settings['api_key']);
+                } else {
+                    echo '<div class="error">
+                            <p>Your shop url is wrong.</p>
+                        </div> ';
+                }
+                
             }
 
             // Handle integration setup
