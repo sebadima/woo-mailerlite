6e7c8a92e0ce72fe9875456ee18c285e96ffaa04
Nikol Petrunova <npetrunova@gmail.com>
2019-11-28 13:38:12 +0200

    margins removed

--- includes/admin/meta-boxes.php
+++ includes/admin/meta-boxes.php
@@ -29,27 +29,27 @@ function woo_ml_order_meta_box_output( $post ) {
     ?>
     <?php $subscribe = woo_ml_order_customer_subscribe( $order_id ); ?>
     <p>
-        <?php _e('Signed up for mailing list:', 'woo-mailerlite' ); ?> <?php echo wc_help_tip("Customer ticked the Subscribe box at the checkout stage to receive newsletters.");?> <?php echo ( $subscribe ) ? $icon_yes : $icon_no; ?>
+        <?php echo _e('Signed up for emails', 'woo-mailerlite' ).wc_help_tip("Customer ticked the Subscribe box at the checkout stage to receive newsletters.");?><?php echo ( $subscribe ) ? $icon_yes : $icon_no; ?>
     </p>
     <?php $subscribed = woo_ml_order_customer_subscribed( $order_id ); ?>
     <p>
-        <?php _e('Subscribed  to mailing list:', 'woo-mailerlite' ); ?> <?php echo wc_help_tip("Customer was successfully added to the subscriber list in MailerLite.");?> <?php echo ( $subscribed ) ? $icon_yes : $icon_no; ?>
+        <?php echo _e('Subscribed to email list', 'woo-mailerlite' ).wc_help_tip("Customer was successfully added to the subscriber list in MailerLite.");?> <?php echo ( $subscribed ) ? $icon_yes : $icon_no; ?>
     </p>
     <?php $already_subscribed = woo_ml_order_customer_already_subscribed( $order_id ); ?>
     <p>
-        <?php _e('Already in the subscriber list:', 'woo-mailerlite' ); ?> <?php echo wc_help_tip("Customer was already an existing subscriber.");?> <?php echo ( $already_subscribed ) ? $icon_yes : $icon_no; ?>
+        <?php echo _e('Previously subscribed', 'woo-mailerlite' ).wc_help_tip("Customer was already an existing subscriber.");?> <?php echo ( $already_subscribed ) ? $icon_yes : $icon_no; ?>
     </p>
     <?php $subscriber_updated = woo_ml_order_subscriber_updated( $order_id ); ?>
     <p>
-        <?php _e('Updated subscriber data:', 'woo-mailerlite' ); ?> <?php echo wc_help_tip("Checkout data including purchases and contact information was successfully updated in MailerLite.");?> <?php echo ( $subscriber_updated ) ? $icon_yes : $icon_no; ?>
+        <?php echo _e('Updated subscriber data', 'woo-mailerlite' ).wc_help_tip("Checkout data including purchases and contact information was successfully updated in MailerLite.");?> <?php echo ( $subscriber_updated ) ? $icon_yes : $icon_no; ?>
     </p>
     <?php $order_data_submitted = woo_ml_order_data_submitted( $order_id ); ?>
     <p>
-        <?php _e('Order data submitted:', 'woo-mailerlite' ); ?> <?php echo wc_help_tip("Order data is uploaded to MailerLite once the payment is processed.");?> <?php echo ( $order_data_submitted ) ? $icon_yes : $icon_no; ?>
+        <?php echo _e('Order data submitted', 'woo-mailerlite' ).wc_help_tip("Order data is uploaded to MailerLite once the payment is processed.");?> <?php echo ( $order_data_submitted ) ? $icon_yes : $icon_no; ?>
     </p>
     <?php $order_tracking_completed = woo_ml_order_tracking_completed( $order_id ); ?>
     <p>
-        <?php _e('Order tracking completed:', 'woo-mailerlite' ); ?> <?php echo wc_help_tip("Tracking data for this order is now available in MailerLite.");?> <?php echo ( $order_tracking_completed ) ? $icon_yes : $icon_no; ?>
+        <?php echo _e('Order tracking completed', 'woo-mailerlite' ).wc_help_tip("Tracking data for this order is now available in MailerLite.");?> <?php echo ( $order_tracking_completed ) ? $icon_yes : $icon_no; ?>
     </p>
     <?php
 }
