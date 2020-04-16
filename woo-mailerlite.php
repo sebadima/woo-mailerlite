f39d1bcaf364262c0a0898cd42537f9a5bb87b8d
dominykasmailer <45770777+dominykasmailer@users.noreply.github.com>
2020-04-16 21:31:26 +0300

    Add WooCommerce minimum required version

--- woo-mailerlite.php
+++ woo-mailerlite.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Plugin Name:     WooCommerce - MailerLite
- * Plugin URI:      https://wordpress.org/plugins/woo-mailerlite/
- * Description:     Official MailerLite integration for WooCommerce. Track sales and campaign ROI, import products details, automate emails based on purchases and seamlessly add your customers to your email marketing lists via WooCommerce's checkout process.
- * Version:         1.4.7
- * Author:          MailerLite
- * Author URI:      https://mailerlite.com
- * Text Domain:     woo-mailerlite
+ * Plugin Name: WooCommerce - MailerLite
+ * Plugin URI: https://wordpress.org/plugins/woo-mailerlite/
+ * Description: Official MailerLite integration for WooCommerce. Track sales and campaign ROI, import products details, automate emails based on purchases and seamlessly add your customers to your email marketing lists via WooCommerce's checkout process.
+ * Version: 1.4.7
+ * WC requires at least: 3.4.0
+ * Author: MailerLite
+ * Author URI: https://mailerlite.com
+ * Text Domain: woo-mailerlite
  */
 
 // Exit if accessed directly
